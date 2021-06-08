0842d28172ade3bd5f34f91d3f6f8d1acce25aa8
arunjos2000 <arunjoseph.14.aj@gmail.com>
2021-06-09 00:28:44 +0530

    few changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var randomImageSource="images/"+randomDiceImage;
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
-var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomNumber2=Math.floor(Math.random()*6)+1;//for image 2
 var randomDiceImage2="dice"+randomNumber2+".png";
 var randomImageSource2="images/"+randomDiceImage2;
 var image2=document.querySelectorAll("img")[1];
