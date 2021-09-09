1f09db6ca1edb86ec2bcc6e2be3955139a3b1324
arunjos2000 <arunjoseph.14.aj@gmail.com>
2021-09-09 19:40:05 +0530

    small changes made

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ image1.setAttribute("src", randomImageSource);
 var randomNumber2=Math.floor(Math.random()*6)+1;//for image 2
 var randomDiceImage2="dice"+randomNumber2+".png";
 var randomImageSource2="images/"+randomDiceImage2;
+
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomImageSource2);
+
 if(randomNumber>randomNumber2){
   document.querySelector("h1").innerHTML="Player 1 Wins";
 }
